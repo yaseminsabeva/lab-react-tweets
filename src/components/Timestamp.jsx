d210aa7ea4642c3b0144aab88bf9994263da9768
Yasemin Sabeva <yaseminsabeva@yahoo.com>
2022-11-21 16:34:03 +0100

    done

---
+++ src/components/Timestamp.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+// const Timestamp = (props) => {
+//     return(
+//         <span className="timestamp">{props.time}</span>
+//     )
+// }
+
+function Timestamp({time}) {
+    return(
+        <span className="timestamp">{time}</span>
+    )
+}
+
+export default Timestamp
